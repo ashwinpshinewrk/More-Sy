d1f94683a125ad08b2ea9b40baa338087535131b
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-08-02 03:51:51 +0530

    feat(app): Knows numbers now

--- More-sy.ino
+++ More-sy.ino
@@ -17,13 +17,12 @@ std::map<String, int> wordToInt = {
 
 
 const auto DOT_DASH_THRES = 300;
-const auto LETTER_PAUSE = 10000;
+const auto LETTER_PAUSE = 1000;
 const auto WORD_PAUSE = 2000; // 2 sec
 
 const int TOUCH_PIN = 23; //D23
 
 bool isTouching = false;
-bool spaceSent = false;
 
 
 unsigned long touchStart = 0; //Timer 0
@@ -66,33 +65,66 @@ void loop() {
     }
   }
 
-  if(!isTouching && lastTouch > 0 ){
-    unsigned long idleTime =  now - lastTouch ;
-    if(idleTime >= WORD_PAUSE){
-      if (morseSymbl.length()){
-        if(morseMap.count(morseSymbl)) {
-          decodedWord += morseMap[morseSymbl];
-        }else {
-          decodedWord = "?";
-        }
-        morseSymbl = "";
-      }
-      Serial.println(decodedWord);
-      lastTouch = now;
-    }else if (idleTime >= LETTER_PAUSE && morseSymbl.length()){
-      if (morseMap.count(morseSymbl)){
-        char c = morseMap[morseSymbl];
-        Serial.print(c);
-        decodedWord += c;
-      }else {
-        Serial.print('?');
-        decodedWord += "?";
-      }
-      morseSymbl = "";
-      lastTouch = now;
+  // if(!isTouching && lastTouch > 0 ){
+  //   unsigned long idleTime =  now - lastTouch ;
+  //   if(idleTime >= WORD_PAUSE){
+  //     if (morseSymbl.length()){
+  //       if(morseMap.count(morseSymbl)) {
+  //         decodedWord += morseMap[morseSymbl];
+  //       }else {
+  //         decodedWord = "?";
+  //       }
+  //       morseSymbl = "";
+  //     }
+  //     Serial.println(decodedWord);
+  //     lastTouch = now;
+  //   }else if (idleTime >= LETTER_PAUSE && morseSymbl.length()){
+  //     if (morseMap.count(morseSymbl)){
+  //       char c = morseMap[morseSymbl];
+  //       Serial.print(c);
+  //       decodedWord += c;
+  //     }else {
+  //       Serial.print('?');
+  //       decodedWord += "?";
+  //     }
+  //     morseSymbl = "";
+  //     lastTouch = now;
+  //   }
+  // }
+
+  if(!isTouching && morseSymbl.length() > 0 && (now - lastTouch >= LETTER_PAUSE)){
+    char letter = '?';
+    if(morseMap.count(morseSymbl)){
+      letter = morseMap[morseSymbl];
     }
+    Serial.print("-> ");
+    Serial.println(letter);
+    decodedWord += letter;
+    morseSymbl = "";
+    lastTouch = now;
   }
 
+  if(!isTouching && decodedWord.length() > 0 && (now - lastTouch >= WORD_PAUSE)){
+    Serial.print("\nWord: ");
+    Serial.println(decodedWord);
+
+    String W = decodedWord;
+
+    W.toUpperCase();
+
+    if(wordToInt.count(W)){
+      Serial.print("Number : ");
+      Serial.println(wordToInt[W]);
+    }else {
+      Serial.println("INAVLDIUE");
+    }
+
+    decodedWord = "";
+    morseSymbl = "";
+    lastTouch = now;
+    Serial.println("\nReady for next..");
+  }
+  
   delay(1);
 
 }
