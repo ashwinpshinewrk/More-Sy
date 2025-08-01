b0646033bc8a813c587376cfe96d12f8cb1077ec
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-08-02 03:21:52 +0530

    Morse can be tranlated now

--- More-sy.ino
+++ More-sy.ino
@@ -2,19 +2,23 @@
 
 //MAP EVERYTHING , Use ChatGPT, I ain't not gonna type everything
 std::map<String, char> morseMap = {
-  {",-",'A'}, {"-...",'B'},{"-.-.",'C'},{"-..",'D'},{".",'E'},{"..-,",'F'},
+  {".-",'A'}, {"-...",'B'},{"-.-.",'C'},{"-..",'D'},{".",'E'},{"..-.",'F'},
   {"--.",'G'},{"....",'H'},{"..",'I'},{".---",'J'},{"-.-",'K'}, {".-..",'L'},{"--", 'M'}, {"-.", 'N'}, {"---", 'O'}, {".--.", 'P'},
   {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'},
   {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'},
   {"-.--", 'Y'}, {"--..", 'Z'}
-}
+};
 
 std::map<String, int> wordToInt = {
   {"ZERO",0}, {"ONE", 1}, {"TWO", 2}, {"THREE", 3},
   {"FOUR", 4}, {"FIVE", 5}, {"SIX", 6}, {"SEVEN", 7},
   {"EIGHT", 8}, {"NINE", 9}, {"TEN", 10}
-}
+};
 
+
+const auto DOT_DASH_THRES = 300;
+const auto LETTER_PAUSE = 10000;
+const auto WORD_PAUSE = 2000; // 2 sec
 
 const int TOUCH_PIN = 23; //D23
 
@@ -26,47 +30,69 @@ unsigned long touchStart = 0; //Timer 0
 unsigned long lastTouch = 0; //Timer 1
 
 
+String morseSymbl = "";
+String decodedWord = "";
+
 void setup() {
   Serial.begin(115200);
   pinMode(TOUCH_PIN,INPUT);
+  Serial.println("Ready");
 
 }
 
 void loop() {
+
   int touchState = digitalRead(TOUCH_PIN);
+  unsigned long now = millis();
 
   if(touchState == HIGH && !isTouching){
     isTouching = true;
-    touchStart = millis(); //Get milli secs
-    spaceSent = false;
+    touchStart = now;
   }
 
   if(touchState == LOW && isTouching){
     isTouching = false;
-    unsigned long timeTaken = millis() - touchStart;
-    lastTouch = millis();
+    unsigned long timeTaken = now - touchStart;
+    lastTouch = now;
 
    // Serial.print(timeTaken);
     //Serial.println(" ms. ");
-    if (timeTaken < 300) {
+    if (timeTaken < DOT_DASH_THRES) {
       Serial.print(".");
+      morseSymbl += ".";
     }else {
       Serial.print("-");
+      morseSymbl += "-";
     }
   }
 
   if(!isTouching && lastTouch > 0 ){
-    unsigned long times =  millis() - lastTouch ;
-    if(times >= 1500 && times < 3000 && !spaceSent){
-      Serial.println(" "); //Space never prints, if it works dont touch
-      spaceSent = true;
-    }else if (times >= 3000 && spaceSent){
-      Serial.print("\n");
-      spaceSent = false;
-      lastTouch = 0; //dont repeat end word 
+    unsigned long idleTime =  now - lastTouch ;
+    if(idleTime >= WORD_PAUSE){
+      if (morseSymbl.length()){
+        if(morseMap.count(morseSymbl)) {
+          decodedWord += morseMap[morseSymbl];
+        }else {
+          decodedWord = "?";
+        }
+        morseSymbl = "";
+      }
+      Serial.println(decodedWord);
+      lastTouch = now;
+    }else if (idleTime >= LETTER_PAUSE && morseSymbl.length()){
+      if (morseMap.count(morseSymbl)){
+        char c = morseMap[morseSymbl];
+        Serial.print(c);
+        decodedWord += c;
+      }else {
+        Serial.print('?');
+        decodedWord += "?";
+      }
+      morseSymbl = "";
+      lastTouch = now;
     }
   }
 
-  delay(10);
+  delay(1);
 
 }
