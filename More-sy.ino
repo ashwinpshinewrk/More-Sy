8bdcf02feb63574bb8da833d8aa83682989d2679
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-08-02 01:39:51 +0530

    (app): Short or long

--- More-sy.ino
+++ More-sy.ino
@@ -1,9 +1,36 @@
+
+const int TOUCH_PIN = 23; //D23
+
+bool isTouching = false;
+
+unsigned long touchStart = 0; //Timer 0
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(TOUCH_PIN,INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int touchState = digitalRead(TOUCH_PIN);
+
+  if(touchState == HIGH && !isTouching){
+    isTouching = true;
+    touchStart = millis(); //Get milli secs
+  }
+
+  if(touchState == LOW && isTouching){
+    isTouching = false;
+    unsigned long timeTaken = millis() - touchStart;
+
+   // Serial.print(timeTaken);
+    //Serial.println(" ms. ");
+    Serial.print("Registered as ");
+    if (timeTaken < 300) {
+      Serial.println(" short.");
+    }else {
+      Serial.println(" long.");
+    }
+  }
 
 }
